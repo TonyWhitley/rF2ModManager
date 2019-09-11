62b2d456b56ea5c2f596c7f33cee9aaca2214dab
TonyWhitley <Tony_Whitley@hotmail.com>
2019-09-11 19:33:33 +0100

    New, simpler method

--- rF2ModManager.py
+++ rF2ModManager.py
@@ -1,61 +1,27 @@
 """
-Install:
-This only has to be done (requiring Admin rights for the symlinks) once.
+New, simpler method:
+The moment rFactor 2.exe starts running move Installed\Locations and Vehicles
+somewhere else.  Take the list of tracks/cars (to be precise, their folders)
+and move them *back* to Installed.
 
-Move Installed\Locations and Installed\Vehicles
-to Userdata\RedirectedContent\Locations & Vehicles
+When rF2 goes to read data it will find only the selected mods.
 
-Set up symlinks replacing Installed\Locations and Installed\Vehicles
-pointing to Userdata\RedirectedContent\Locations & Vehicles
-(in there is a symlink to Userdata\SelectedContent)
+The moment rFactor 2.exe stops running, move the remainder back Installed and
+things are back as they were.
 
-There is also Userdata\SelectedContent\Locations & Vehicles
-
-In use:
-Given a new list of tracks/cars (to be precise, their folders)
-* move any folders in SelectedContent back to RedirectedContent
-* move the list from RedirectedContent to SelectedContent
-
-The moment rFactor 2.exe starts running, if SelectedContent is not empty
-* rename RedirectedContent to RedirectedContent.IDLE
-* rename SelectedContent to RedirectedContent
-
-When rF2 goes to read data it will follow the symlink to
-Userdata\RedirectedContent which is now Userdata\SelectedContent
-
-The moment rFactor 2.exe stops running, if RedirectedContent.IDLE exists
-* rename RedirectedContent back to SelectedContent
-* rename RedirectedContent.IDLE to RedirectedContent
-
-So the directory structure is:
-Userdata\RedirectedContent\Locations\track1
-                                    \track2 etc.
-                           Vehicles\car1
-                                   \car2 etc.
-
-Userdata\SelectedContent\Locations\track3
-                                  \track4 etc.
-                         Vehicles\car3
-                                 \car4 etc.
-
-and those two swap over when rF2 is running then back when it stops running.
-
-Also have a repair function that
-if RedirectedContent.IDLE exists
-* rename RedirectedContent back to SelectedContent
-* rename RedirectedContent.IDLE to RedirectedContent
-* move any folders in SelectedContent back to RedirectedContent
+Also have a repair function that moves everything back to Installed
 
 """
 from pathlib import Path
 import psutil
+from subprocess import Popen, call
 import sys
 from time import sleep
 from _winapi import CreateJunction
 
 from configIni import Config
 
-class rF2:
+class rF2_check:
     rf2_pid = None          # Once we've found rF2 running
     rf2_pid_counter = 0     # Counter to check if running
     rf2_running = False
@@ -110,36 +76,22 @@ class rF2:
 class Mod_manager():
     def __init__(self, rf2path):
         self.rf2dir = Path(rf2path).expanduser()
-        self.all_content = self.rf2dir.joinpath('Userdata')\
+        self.content_store = self.rf2dir.joinpath('Userdata')\
             .joinpath('ContentStorage')
-        self.redirected_content = self.all_content.joinpath('RedirectedContent')
         self.installed = self.rf2dir.joinpath('Installed')
-        self.selected_content = self.all_content.joinpath('SelectedContent')
         if not self.rf2dir.is_dir():
             return None
-        if not self.redirected_content.is_dir():
-            self.install()
 
-    def install(self):
+    def setup(self):
         """
         Move Installed\Locations and Installed\Vehicles
-        to Userdata\RedirectedContent\Locations & Vehicles
-
-        Set up symlinks replacing Installed\Locations and Installed\Vehicles
-        pointing to Userdata\RedirectedContent\Locations & Vehicles
-        (in there is a symlink to Userdata\SelectedContent)
-
-        There is also Userdata\SelectedContent\Locations & Vehicles
+        to Userdata\ContentStorage\Locations & Vehicles
         """
 
-        _sc_locations = self.selected_content.joinpath('Locations')
-        _sc_vehicles = self.selected_content.joinpath('Vehicles')
+        _cs_locations = self.content_store.joinpath('Locations')
+        _cs_vehicles = self.content_store.joinpath('Vehicles')
         try:
-            self.all_content.mkdir(parents=False, exist_ok=False)
-            self.redirected_content.mkdir(parents=False, exist_ok=False)
-            self.selected_content.mkdir(parents=False, exist_ok=False)
-            _sc_locations.mkdir(parents=False, exist_ok=False)
-            _sc_vehicles.mkdir(parents=False, exist_ok=False)
+            self.content_store.mkdir(parents=False, exist_ok=True)
         except FileNotFoundError:
             raise FileNotFoundError
         except FileExistsError:
@@ -148,124 +100,82 @@ class Mod_manager():
             print(e)
             raise e
         # Move Installed\Locations and Installed\Vehicles
-        # to Userdata\RedirectedContent\Locations & Vehicles
+        # to Userdata\ContentStorage\Locations & Vehicles
         _locations = self.installed.joinpath('Locations')
         _vehicles = self.installed.joinpath('Vehicles')
-        _rc_locations = self.redirected_content.joinpath('Locations')
-        _rc_vehicles = self.redirected_content.joinpath('Vehicles')
-        _locations.rename(_rc_locations)
-        _vehicles.rename(_rc_vehicles)
+        _locations.rename(_cs_locations)
+        _vehicles.rename(_cs_vehicles)
 
-        # symlinks (needs admin) vs. junctions (don't)
-        junction = str(_locations)  # Link
-        src = str(_rc_locations)    # points to
-        #src = str(self.redirected_content)
-        CreateJunction(src, junction)
-
-        junction = str(_vehicles)       # Link
-        src = str(_rc_vehicles)    # points to
-        #src = str(self.redirected_content)
-        CreateJunction(src, junction)
-
-        #self.installed.joinpath('Locations').symlink_to(_locations)
-        #self.installed.joinpath('Vehicles').symlink_to(_vehicles)
 
     def select_mod(self, mod):
         """
         Given a new list of tracks/cars (to be precise, their folders)
-        * move the list from RedirectedContent to SelectedContent
+        * move the list from ContentStorage to Installed
         """
-        _rc_mods = self.redirected_content.joinpath(mod[0])
-        _sc_mods = self.selected_content.joinpath(mod[0])
-        _rc_mod = _rc_mods.joinpath(mod[1])
-        _sc_mod = _sc_mods.joinpath(mod[1])
-        if not _rc_mod.is_dir():
+        _ins_path = self.installed.joinpath(mod[0])
+        _cs_path = self.content_store.joinpath(mod[0])
+        _ins_mod = _ins_path.joinpath(mod[1])
+        _cs_mod = _cs_path.joinpath(mod[1])
+        if not _cs_mod.is_dir():
             return None
-        _rc_mod.rename(_sc_mod)
+        if not _ins_path.is_dir():
+            _ins_path.mkdir(parents=True, exist_ok=True)
+        _cs_mod.rename(_ins_mod)
         return mod
 
     def unselect_mods(self):
         """
-        Move any folders in SelectedContent back to RedirectedContent
+        Move any folders in ContentStorage back to Installed
         """
         for _type in ['Locations', 'Vehicles']:
-            for _sc_mod in self.selected_content.joinpath(_type).iterdir():
-                _rc_path = self.redirected_content.joinpath(_type)
-                _rc_mod = _rc_path.joinpath(_sc_mod.name)
-                _sc_mod.rename(_rc_mod)
+            for _cs_mod in self.content_store.joinpath(_type).iterdir():
+                _ins_path = self.installed.joinpath(_type)
+                _ins_mod = _ins_path.joinpath(_cs_mod.name)
+                _cs_mod.rename(_ins_mod)
+            # Tidy up, remove the directory
+            self.content_store.joinpath(_type).rmdir()
+        self.content_store.rmdir()
 
-    def use_selected_content(self):
-        """
-        If SelectedContent is not empty
-        * rename RedirectedContent to RedirectedContent.IDLE
-        * rename SelectedContent to RedirectedContent
-        """
-        if self.selected_content.glob('**'):
-            # SelectedContent is not empty
-            _new_name = self.redirected_content.parent.joinpath(
-                self.redirected_content.name+'.IDLE')
-            self.redirected_content.rename(_new_name)
-            self.selected_content.rename(self.redirected_content)
-
-    def stop_using_selected_content(self):
-        """
-        If RedirectedContent.IDLE exists
-        * rename RedirectedContent back to SelectedContent
-        * rename RedirectedContent.IDLE to RedirectedContent
-        """
-        _new_name = self.redirected_content.parent.joinpath(
-            self.redirected_content.name+'.IDLE')
-        if _new_name.exists():
-            self.redirected_content.rename(self.selected_content)
-            _new_name.rename(self.redirected_content)
-            return True
-        return False
 
     def repair(self):
         """
-        if RedirectedContent.IDLE exists
-        * rename RedirectedContent back to SelectedContent
-        * rename RedirectedContent.IDLE to RedirectedContent
-        * move any folders in SelectedContent back to RedirectedContent
+        * move any folders in SelectedContent back to Installed
         """
-        if self.stop_using_selected_content():
-            self.unselect_mods()
+        self.unselect_mods()
 
 def main():
     _CONFIG_O = Config(sys.argv[1])
     rf2path = _CONFIG_O.get_rfactor_folder()
-    #rf2path = r'c:\Temp\dummyRf2'
+    #rf2path = r'c:\Temp\dummy Rf2'
     mm_o = Mod_manager(rf2path)
     if not mm_o:
         SystemExit(99)
 
-    for location in _CONFIG_O.get_locations():
-        if 1:# not mm_o.select_mod('Locations', location):
-            print(f'Locations {location} not found')
+    rf2_o = rF2_check()
 
-    for vehicle in _CONFIG_O.get_vehicles():
-        if 1:# not mm_o.select_mod('Vehicles', vehicle):
-            print(f'Vehicles {vehicle} not found')
-
-    """
-    Live!
-    if not mm_o.select_mod(('Locations', '60sHockenheim')):
-        print('Locations 60sHockenheim not found')
-    if not mm_o.select_mod(('Vehicles', 'Ferrari_312_67')):
-        print('Vehicles Ferrari_312_67 not found')
-    """
-
-    rf2_o = rF2()
+    #call('"c:/Program Files(x86)/Steam/steam" -applaunch 365960')
+    print('\nWaiting for rFactor 2 to start...')
 
     while not rf2_o.isRF2running():
         sleep(.2)
 
-    mm_o.use_selected_content()
+    print('\nrFactor 2 started')
+    mm_o.setup()
+    for location in _CONFIG_O.get_locations():
+        if not mm_o.select_mod(('Locations', location)):
+            print(f'Locations {location} not found')
 
+    for vehicle in _CONFIG_O.get_vehicles():
+        if not mm_o.select_mod(('Vehicles', vehicle)):
+            print(f'Vehicles {vehicle} not found')
+
+
+    print('\nWaiting for rFactor 2 to stop...')
     while rf2_o.isRF2running():
         sleep(1)
 
-    mm_o.stop_using_selected_content()
+    print('\nrFactor 2 stopped')
+
     #mm_o.repair()
     mm_o.unselect_mods()
 
